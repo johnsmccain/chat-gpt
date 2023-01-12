9cc461f2d7baa9fc15024b5742e1a5cadccafc0e
johnsmccain <johnsdanlami@gmail.com>
2023-01-12 03:22:33 +0100

    chat gpt

--- server/index.js
+++ server/index.js
@@ -23,7 +23,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-console.log(process.env.OPENAI_KEY)
 
 app.get("/", async(req, res) => {
     res.status(200).json("Hello World!");
@@ -32,17 +31,7 @@ app.get("/", async(req, res) => {
 app.post('/', async(req, res) => {
     try {
         const prompt = req.body.prompt;
-        console.log(prompt)
-        console.log()
-        // const response = await openAi.createCompletion({
-        //     model: "text-davinci-003",
-        //     prompt: `${prompt}`,
-        //     temperature: 0,
-        //     max_tokens: 3000,
-        //     top_p: 1,
-        //     frequency_penalty: 0.5,
-        //     present_penalty: 0,
-        // });
+
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt,
